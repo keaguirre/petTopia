719fe64e4ef75b704fb9c148c0ef4e4c21190f67
keaguirre <ke.aguirre7@gmail.com>
2025-05-16 04:51:37 -0400

    add base pages + dependencies installed [tailwind,daisyui,clerk]

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true, // reload for wsl2
+      },
+    },
+  },
+  adapter: node({ mode: "standalone" }),
+  output: "server",
+  integrations: [clerk()],
+});
