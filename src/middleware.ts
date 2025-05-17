0b68f4b2050ab796903002f276c38f09c0d96ce3
keaguirre <ke.aguirre7@gmail.com>
2025-05-17 06:14:44 -0400

    changed home.astro to play.astro, optimized imgs, play.astro protected route, better headers on ML

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
-const isProtectedRoute = createRouteMatcher(['/home(.*)', '/settings', '/profile']);
+const isProtectedRoute = createRouteMatcher(['/play(.*)', '/settings', '/profile']);
 export const onRequest = clerkMiddleware((auth, context) => {
     const { userId, redirectToSignIn } = auth();
 
